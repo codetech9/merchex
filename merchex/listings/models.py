5cf33dd1e12faff22e33864509816ebce2eabba9
Moïse GUANNEL <mguannel97@gmail.com>
2022-06-07 10:38:01 -0400

    update CRUD for listings model

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -5,7 +5,7 @@ from  django.core.validators import MaxValueValidator, MinValueValidator
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
     genre = models.fields.CharField(max_length=50)
-    biography = models.fields.CharField(max_length=100)
+    biography = models.fields.CharField(max_length=1000)
     year_formed = models.fields.IntegerField(
         validators=[MinValueValidator(1900),    MaxValueValidator(2022)]
     )
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -20,9 +20,9 @@ from listings import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bands/', views.band_list, name='band-list'),
-    path('bands/<int:id>/', views.band_detail),
     path('bands/<int:id>/', views.band_detail, name='band-detail'),
     path('about-us/', views.about),
-    path('listings/', views.listing),
+    path('listings/', views.listing_list, name='listing-list'),
+    path('listings/<int:id>/', views.listing_detail, name='listing-detail'),
     path('contact-us/', views.contact),
 ]
