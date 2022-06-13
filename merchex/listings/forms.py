fe9259a8c4a789dbec80292c8d458b77674b132f
Moïse GUANNEL <mguannel97@gmail.com>
2022-06-13 12:48:02 -0400

    add contact Form and links for contact-us and about-us page

---
+++ merchex/listings/forms.py
@@ -0,0 +1,9 @@
+import email
+from email import message
+from django import forms
+from listings.models import Band, Listing
+
+class ContactUsForm(forms.Form):
+    name = forms.CharField(required=False)
+    email = forms.EmailField()
+    message = forms.CharField(max_length=1000)
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,9 +1,14 @@
 from cgitb import html
+import re
 from urllib import request
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
 from listings.models import Band
 from listings.models import Listing
+from listings.forms import ContactUsForm
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+
 
 # Create your views here.
 def band_list(request):
@@ -17,7 +22,6 @@ def band_detail(request, id):  # notez le paramètre id supplémentaire
     return render(request,
     'listings/band_detail.html',
     {'band': band}) # nous mettons à jour cette ligne pour passer le groupe au gabarit
-
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p> Nous adorons merch ! </p>')
@@ -36,3 +40,23 @@ def listing_detail(request, id):  # notez le paramètre id supplémentaire
     return render(request,
     'listings/listing_detail.html',
     {"listing": listing}) # nous mettons à jour cette ligne pour passer le groupe au gabarit
+
+def contact(request):
+    # ajoutez ces instructions d'impression afin que nous puissions jeter un coup d'oeil à « request.method » et à « request.POST »
+    if request.method == 'POST':
+     # créer une instance de notre formulaire et le remplir avec les données POST
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            send_mail(
+            subject = f'Message from {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact Us form',
+            message = form.cleaned_data['message'],
+            from_email=form.cleaned_data['email'],
+            recipient_list=['admin@merchex.xyz'],
+            )
+        return redirect('email-envoye')
+    else:
+    # ceci doit être une requête GET, donc créer un formulaire vide
+        form = ContactUsForm()
+    return render(request,
+    'listings/contact.html',
+    {'form': form})
